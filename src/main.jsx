1bd862789874d3fdca71f1f4b51006fede67f3db
jsebasvgg7 <johnsebastianveggonzalez@gmail.com>
2025-11-30 09:43:48 -0500

    v8.4

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import '../styles/Responsive.css'
+import './styles/responsive.css';
 
 createRoot(document.getElementById('root')).render(
 <React.StrictMode>
